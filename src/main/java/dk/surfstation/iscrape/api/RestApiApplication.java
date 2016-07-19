fe473716d3769a2a3027c035af3b12fbb4c33e1f
Sebastian Jensen <sebastianthegreatful@gmail.com>
2016-07-19 23:31:26 +0200

    Tomcat deployable.

--- src/main/java/dk/surfstation/iscrape/api/RestApiApplication.java
+++ src/main/java/dk/surfstation/iscrape/api/RestApiApplication.java
@@ -2,12 +2,19 @@ package dk.surfstation.iscrape.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.boot.orm.jpa.EntityScan;
 
 
 @SpringBootApplication(scanBasePackages = "dk.surfstation.iscrape")
 @EntityScan(basePackages = "dk.surfstation.iscrape.business.domain")
-public class RestApiApplication {
+public class RestApiApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RestApiApplication.class);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiApplication.class, args);
 	}
